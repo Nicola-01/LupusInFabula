90337fc320c6b93a2f2b1166a7c8c6b9b1a1765d
rickm01 <riccardo.miele01@gmail.com>
2024-04-10 14:49:11 +0200

    Creation of the comment for each table

--- src/main/database/game.sql
+++ src/main/database/game.sql
@@ -5,3 +5,10 @@ CREATE TABLE Game (
                       who_wins SMALLINT,
                       number_of_rounds INTEGER
 );
+
+COMMENT ON TABLE Game IS 'Represents a game played.';
+COMMENT ON COLUMN Game.ID IS 'The unique identifier for each game.';
+COMMENT ON COLUMN Game.date IS 'The date when the game was played.';
+COMMENT ON COLUMN Game.hour IS 'The time when the game started.';
+COMMENT ON COLUMN Game.who_wins IS 'The faction that won the game.';
+COMMENT ON COLUMN Game.number_of_rounds IS 'The total number of rounds played in the game.';
--- src/main/database/role.sql
+++ src/main/database/role.sql
@@ -5,3 +5,10 @@ CREATE TABLE Role (
                       with_who_wins SMALLINT NOT NULL,
                       description CHARACTER VARYING
 );
+
+COMMENT ON TABLE Role IS 'Represents different roles in the game.';
+COMMENT ON COLUMN Role.ID IS 'The unique identifier for each role.';
+COMMENT ON COLUMN Role.name IS 'The name of the role.'
+COMMENT ON COLUMN Role.type IS 'The type of the role (good, evil or neutral).'
+COMMENT ON COLUMN Role.with_who_wins IS 'The faction with which the role can win the game (wolfes, farmers or itself).';
+COMMENT ON COLUMN Role.description IS 'A description of the role.';
