0a7f2d8d2fcc3eea8e97cc2b86d5b05ae8974b29
Nicola Pozzo <nicola.pozzo@studenti.unipd.com>
2024-04-11 14:59:51 +0200

    Add Game.java file

---
+++ src/main/java/it/unipd/dei/webapp/lupus/resource/Game.java
@@ -0,0 +1,39 @@
+package it.unipd.dei.webapp.lupus.resource;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Game {
+    private int id;
+    private final Time game_duration;
+    private final String who_win;
+    private final int number_of_rounds;
+    private final Date start;
+
+    public Game(final int id, final Time game_duration, final String who_win,
+                final int number_of_rounds, final Date start) {
+        this.id = id;
+        this.game_duration = game_duration;
+        this.who_win = who_win;
+        this.number_of_rounds = number_of_rounds;
+        this.start = start;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final Time getDuration() {return game_duration; }
+
+    public final String getWho_win() {
+        return who_win;
+    }
+
+    public final int getNumbers_of_round() {
+        return number_of_rounds;
+    }
+
+    public final Date getGameDate() {
+        return start;
+    }
+}
