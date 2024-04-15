3063349c9f30474a21ac3a40f834d1e7e6d05e4a
Nicola Pozzo <nicola.pozzo@studenti.unipd.com>
2024-04-15 10:11:08 +0200

    Auto stash before rebase of "Friend" onto "master"
    Add error control in FriendServlet

--- src/main/java/it/unipd/dei/webapp/lupus/servlet/FriendServlet.java
+++ src/main/java/it/unipd/dei/webapp/lupus/servlet/FriendServlet.java
@@ -5,6 +5,7 @@ import it.unipd.dei.webapp.lupus.resource.Message;
 import it.unipd.dei.webapp.lupus.resource.Player;
 import it.unipd.dei.webapp.lupus.resource.Is_Friend_With;
 
+import it.unipd.dei.webapp.lupus.utils.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -16,8 +17,12 @@ import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class FriendServlet extends AbstractDatabaseServlet{
+
+    String usernameRegex = "^[a-zA-Z0-9](?:[a-zA-Z0-9_]*[a-zA-Z0-9])?$";
+    Pattern usernameRegexPattern = Pattern.compile(usernameRegex);
 
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
@@ -25,19 +30,11 @@ public class FriendServlet extends AbstractDatabaseServlet{
         Message m = null;
 
         try {
-            HttpSession session = req.getSession(false);
-            // check if the user hae done the login
-            if (session != null && session.getAttribute("player") != null) {
-                //take the username of the player
-                Player player = (Player) session.getAttribute("player");
-                friendship = new SearchFriendsByUsernameDAO(getConnection(), player.getUsername()).access().getOutputParam();
-                m = new Message("Correctly find list of friends");
-                LOGGER.info("Correctly find list of friends");
-            } else {
-                LOGGER.info("User is not logged in. Redirecting to login page.");
-                //send the user to the login page
-                req.getRequestDispatcher("/jsp/login.jsp").forward(req, res);
-            }
+            //take the username of the player
+            Player player = (Player) req.getSession().getAttribute("player");
+            friendship = new SearchFriendsByUsernameDAO(getConnection(), player.getUsername()).access().getOutputParam();
+            m = new Message("Correctly find list of friends");
+            LOGGER.info("Correctly find list of friends");
         } catch (SQLException e) { // (SQLException | ServletException e)
             m = new Message("Cannot search for friends: unexpected error while accessing the database.", "E200",
                     e.getMessage());
@@ -59,26 +56,37 @@ public class FriendServlet extends AbstractDatabaseServlet{
         Message m = null;
 
         try {
-            HttpSession session = req.getSession(false);
-            // check if the user hae done the login
-            if (session != null && session.getAttribute("player") != null) {
-                //take the username of the player
-                Player player = (Player) session.getAttribute("player");
-                String friend_username = req.getParameter("friend_username");
-                Date date = new Date(System.currentTimeMillis());
+            //take the username of the player
+            Player player = (Player) req.getSession().getAttribute("player");
+            String friend_username = req.getParameter("friend_username");
+            Date date = new Date(System.currentTimeMillis());
 
+            if (!usernameRegexPattern.matcher(friend_username).matches()) {
+                // TODO: To check
+                ErrorCode ec = ErrorCode.INVALID_USERNAME_FORMAT;
+                res.setStatus(ec.getHTTPCode());
+
+                m = new Message("Friend_username not valid", "" + ec.getErrorCode(), ec.getErrorMessage());
+                req.setAttribute("message", m);
+
+                LOGGER.info("Friend_username not valid");
+            }else if(new SearchPlayerByUsernameDAO(getConnection(), friend_username).access().getOutputParam() == null) {
+                ErrorCode ec = ErrorCode.PLAYER_NOT_EXIST;
+                res.setStatus(ec.getHTTPCode());
+
+                m = new Message("This player doesn't exist", "" + ec.getErrorCode(), ec.getErrorMessage());
+                req.setAttribute("message", m);
+
+                LOGGER.info("This player doesn't exist");
+            }else{
                 int result = new AddFriendDAO(getConnection(), player.getUsername(), friend_username, date).access().getOutputParam();
                 if (result == 1) {
                     m = new Message("Correctly add friend");
                     LOGGER.info("Correctly add friend");
-                }else {
+                } else {
                     m = new Message("No player found");
                     LOGGER.info("No player found");
                 }
-            } else {
-                LOGGER.info("User is not logged in. Redirecting to login page.");
-                //send the user to the login page
-                req.getRequestDispatcher("/jsp/login.jsp").forward(req, res);
             }
         } catch (SQLException e) { // (SQLException | ServletException e)
             m = new Message("Cannot search for friends: unexpected error while accessing the database.", "E200",
@@ -91,24 +99,28 @@ public class FriendServlet extends AbstractDatabaseServlet{
         Message m = null;
 
         try {
-            HttpSession session = req.getSession(false);
-            // check if the user hae done the login
-            if (session != null && session.getAttribute("player") != null) {
-                //take the username of the player
-                Player player = (Player) session.getAttribute("player");
-                String friend_username = req.getParameter("friend_username");
+            //take the username of the player
+            Player player = (Player) req.getSession().getAttribute("player");
+            String friend_username = req.getParameter("friend_username");
+
+            if (!usernameRegexPattern.matcher(friend_username).matches()) {
+                // TODO: To check
+                ErrorCode ec = ErrorCode.INVALID_USERNAME_FORMAT;
+                res.setStatus(ec.getHTTPCode());
+
+                m = new Message("Friend_username not valid", "" + ec.getErrorCode(), ec.getErrorMessage());
+                req.setAttribute("message", m);
+
+                LOGGER.info("Friend_username not valid");
+            }else {
                 int result = new DeleteFriendDAO(getConnection(), player.getUsername(), friend_username).access().getOutputParam();
-                if(result == 1){
+                if (result == 1) {
                     m = new Message("Correctly delete friend");
                     LOGGER.info("Correctly delete friend");
-                }else{
+                } else {
                     m = new Message("No friend found");
                     LOGGER.info("No friend found");
                 }
-            } else {
-                LOGGER.info("User is not logged in. Redirecting to login page.");
-                //send the user to the login page
-                req.getRequestDispatcher("/jsp/login.jsp").forward(req, res);
             }
         } catch (SQLException e) { // (SQLException | ServletException e)
             m = new Message("Cannot search for friends: unexpected error while accessing the database.", "E200",
