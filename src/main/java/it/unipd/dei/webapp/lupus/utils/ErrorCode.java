2439dd94771240baf7f59c7933af9fb3e97ef015
rickm01 <riccardo.miele@studenti.unipd.it>
2024-04-28 23:07:27 +0200

    Fixing errorcode

--- src/main/java/it/unipd/dei/webapp/lupus/utils/ErrorCode.java
+++ src/main/java/it/unipd/dei/webapp/lupus/utils/ErrorCode.java
@@ -38,13 +38,13 @@ public enum ErrorCode {
     USER_NOT_FOUND("EUSR9", HttpServletResponse.SC_NOT_FOUND, "User not found"),
 
     // game
-    PLAYER_NOT_EXIST("EGME1", HttpServletResponse.SC_BAD_REQUEST, "One or more players does not exist."),
-    PLAYER_ALREADY_IN_GAME("EGME2", HttpServletResponse.SC_BAD_REQUEST, "One or more players are already in a game."),
-    MASTER_ALREADY_IN_GAME("EGME3", HttpServletResponse.SC_BAD_REQUEST, "The gamemaster is already in a game."),
-    ROLE_NOT_EXIST("EGME4", HttpServletResponse.SC_BAD_REQUEST, "One or more roles does not exist."),
+    PLAYER_NOT_EXIST("EGME1", HttpServletResponse.SC_NOT_FOUND, "One or more players does not exist."),
+    PLAYER_ALREADY_IN_GAME("EGME2", HttpServletResponse.SC_CONFLICT, "One or more players are already in a game."),
+    MASTER_ALREADY_IN_GAME("EGME3", HttpServletResponse.SC_CONFLICT, "The gamemaster is already in a game."),
+    ROLE_NOT_EXIST("EGME4", HttpServletResponse.SC_NOT_FOUND, "One or more roles does not exist."),
     NUMBER_PLAYERS_ROLES_NOT_MATCH("EGME5", HttpServletResponse.SC_BAD_REQUEST, "Number of players entered does not correspond to the number of roles."),
     NOT_ENOUGH_PLAYERS("EGME6", HttpServletResponse.SC_BAD_REQUEST, "Not enough players."),
-    INVALID_GAMESETTINGS("EGME7", HttpServletResponse.SC_BAD_REQUEST, "The parameter does not exist."),
+    INVALID_GAMESETTINGS("EGME7", HttpServletResponse.SC_NOT_FOUND, "The parameter does not exist."),
     INVALID_ROLES_CARDINALITY("EGME8", HttpServletResponse.SC_BAD_REQUEST, "Invalid role max cardinality."),
 
     GAME_NOT_FOUND("EGME9", HttpServletResponse.SC_NOT_FOUND, "Game not found."),
