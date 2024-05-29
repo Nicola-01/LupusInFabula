2964a26c17fed724b8a9d039e2b43aa79ec3b866
JapoMomi1 <jacopo.momesso@studenti.unipd.it>
2024-05-29 20:58:47 +0200

    Added passwords and email complexity check on the update

--- src/main/java/it/unipd/dei/webapp/lupus/rest/UserMePutRR.java
+++ src/main/java/it/unipd/dei/webapp/lupus/rest/UserMePutRR.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.regex.Pattern;
 
 /**
  * Represents a REST resource handler for handling PUT requests related to updating the current user's account.
@@ -30,6 +31,31 @@ import java.sql.SQLException;
  * @since 1.0
  */
 public class UserMePutRR extends AbstractRR {
+
+    /**
+     * Represents the regular expression pattern to validate email addresses.
+     * The email address must have the format local_part@domain.
+     * It must contain at least one character before the "@" symbol,
+     * followed by the "@" symbol, and then the domain name with at least one dot.
+     */
+    String emailRegex = "^((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$";
+
+    /**
+     * The compiled pattern for validating email addresses.
+     */
+    Pattern emailRegexPattern = Pattern.compile(emailRegex);
+
+    /**
+     * Represents the regular expression pattern to validate passwords.
+     * Passwords must be between 8 and 16 characters long and contain at least one uppercase letter,
+     * one lowercase letter, one number, and one special character.
+     */
+    String passwordRegex = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$";
+
+    /**
+     * The compiled pattern for validating passwords.
+     */
+    Pattern passwordRegexPattern = Pattern.compile(passwordRegex);
 
     /**
      * Constructs a new UserMePutRR object with the specified HTTP request, response, and data source.
@@ -73,26 +99,39 @@ public class UserMePutRR extends AbstractRR {
                 String newPassword = user_update.getNewPassword();
                 String repeatNewPassword = user_update.getRepeatNewPassword();
                 //LOGGER.info("Username: " + user + " --> is trying to update the password");
+                Message m;
 
-                if (newPassword.equals(repeatNewPassword)) {
+                if (passwordRegexPattern.matcher(newPassword).matches()) {
 
-                    int rs = new UpdatePasswordByUsernameDAO(ds.getConnection(), user, oldPassword, newPassword).access().getOutputParam();
-                    //LOGGER.info("The new password and the repeatNewPassword are the same");
-                    Message m;
+                    if (newPassword.equals(repeatNewPassword)) {
 
-                    if (rs == 1) {
+                        int rs = new UpdatePasswordByUsernameDAO(ds.getConnection(), user, oldPassword, newPassword).access().getOutputParam();
+                        //LOGGER.info("The new password and the repeatNewPassword are the same");
 
-                        m = new Message("User has successfully updated the password");
-                        LOGGER.info("User " + user + " has successfully updated the password");
-                        res.setStatus(HttpServletResponse.SC_OK);
-                        m.toJSON(res.getOutputStream());
+                        if (rs == 1) {
+
+                            m = new Message("User has successfully updated the password");
+                            LOGGER.info("User " + user + " has successfully updated the password");
+                            res.setStatus(HttpServletResponse.SC_OK);
+                            m.toJSON(res.getOutputStream());
+
+                        } else {
+
+                            LOGGER.error("Update password failed");
+
+                            ErrorCode ec = ErrorCode.UPDATE_NOT_SUCCESSFUL;
+                            m = new Message("User " + user + " failed to update the password", ec.getErrorCode(), ec.getErrorMessage());
+                            res.setStatus(ec.getHTTPCode());
+                            m.toJSON(res.getOutputStream());
+
+                        }
 
                     } else {
 
-                        LOGGER.error("Update password failed");
+                        LOGGER.error("New password and repeatNewPassword do not match");
 
-                        ErrorCode ec = ErrorCode.UPDATE_NOT_SUCCESSFUL;
-                        m = new Message("User " + user + " failed to update the password", ec.getErrorCode(), ec.getErrorMessage());
+                        ErrorCode ec = ErrorCode.PASSWORD_NOT_MATCH;
+                        m = new Message("New password and repeatNewPassword do not match", ec.getErrorCode(), ec.getErrorMessage());
                         res.setStatus(ec.getHTTPCode());
                         m.toJSON(res.getOutputStream());
 
@@ -100,10 +139,10 @@ public class UserMePutRR extends AbstractRR {
 
                 } else {
 
-                    LOGGER.error("New password and repeatNewPassword do not match");
+                    LOGGER.error("Passwords not complex enough");
 
-                    ErrorCode ec = ErrorCode.PASSWORD_NOT_MATCH;
-                    Message m = new Message("New password and repeatNewPassword do not match", ec.getErrorCode(), ec.getErrorMessage());
+                    ErrorCode ec = ErrorCode.INVALID_PASSWORD_FORMAT;
+                    m = new Message("Passwords not complex enough", ec.getErrorCode(), ec.getErrorMessage());
                     res.setStatus(ec.getHTTPCode());
                     m.toJSON(res.getOutputStream());
 
@@ -116,21 +155,34 @@ public class UserMePutRR extends AbstractRR {
                 String newEmail = user_update.getNewEmail();
                 //LOGGER.info("Username: " + user + " --> trying to update the email");
                 int rs = new UpdateEmailByUsernameDAO(ds.getConnection(), user, oldEmail, newEmail).access().getOutputParam();
-
                 Message m;
-                if (rs == 1) {
 
-                    m = new Message("User has successfully updated the email");
-                    LOGGER.info("Player " + user + "'s successfully updated the old email " + oldEmail + " to the new one " + newEmail);
-                    res.setStatus(HttpServletResponse.SC_OK);
-                    m.toJSON(res.getOutputStream());
+                if (emailRegexPattern.matcher(newEmail).matches()) {
+
+                    if (rs == 1) {
+
+                        m = new Message("User has successfully updated the email");
+                        LOGGER.info("Player " + user + "'s successfully updated the old email " + oldEmail + " to the new one " + newEmail);
+                        res.setStatus(HttpServletResponse.SC_OK);
+                        m.toJSON(res.getOutputStream());
+
+                    } else {
+
+                        LOGGER.error("Impossible to update the old email to the new one");
+
+                        ErrorCode ec = ErrorCode.EMAIL_ALREADY_USED;
+                        m = new Message("Impossible to update the old email to the new one", ec.getErrorCode(), ec.getErrorMessage());
+                        res.setStatus(ec.getHTTPCode());
+                        m.toJSON(res.getOutputStream());
+
+                    }
 
                 } else {
 
-                    LOGGER.error("Impossible to update the old email to the new one");
+                    LOGGER.error("Email not valid");
 
-                    ErrorCode ec = ErrorCode.EMAIL_ALREADY_USED;
-                    m = new Message("Impossible to update the old email to the new one", ec.getErrorCode(), ec.getErrorMessage());
+                    ErrorCode ec = ErrorCode.INVALID_EMAIL_FORMAT;
+                    m = new Message("Email not valid", ec.getErrorCode(), ec.getErrorMessage());
                     res.setStatus(ec.getHTTPCode());
                     m.toJSON(res.getOutputStream());
 
