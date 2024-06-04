b91b80bf5f21696a04baaff46cc8fc8b6879686f
Nicola <nickbusato0101@gmail.com>
2024-06-04 22:16:30 +0200

    cookie Fix

--- src/main/webapp/js/game/currentGame/playersStatus.js
+++ src/main/webapp/js/game/currentGame/playersStatus.js
@@ -2,20 +2,6 @@ let playerRole = [];
 
 const maxPlayersForCircularButtons = 12;
 
-// to change card backing
-function getCookie(name)
-{
-    let cookieArr = document.cookie.split(";");
-    for(let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if(name == cookiePair[0].trim())
-        {
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    return null;
-}
-
 function fillPlayersStatus(req) {
     if (req.readyState === XMLHttpRequest.DONE)
     {
--- src/main/webapp/js/user/cards.js
+++ src/main/webapp/js/user/cards.js
@@ -1,22 +1,9 @@
-function getCardCookie(name)
-{
-    let cookieArr = document.cookie.split(";");
-    for(let i = 0; i < cookieArr.length; i++) {
-        let cookiePair = cookieArr[i].split("=");
-        if(name == cookiePair[0].trim())
-        {
-            return decodeURIComponent(cookiePair[1]);
-        }
-    }
-    return null;
-}
-
 function loadImages()
 {
     var imagesFolder = "../media/cards/card_back/";
     var imageContainer = document.getElementById("imageContainer");
     var imageFiles = ["fantasy1.png", "fantasy2.png", "yugioh.png", "standard_red.png", "standard_blue.png", "wooden.png", "japo_lightblue.png", "japo_white.png"];
-    var selectedBack = getCardCookie("selectedCard");
+    var selectedBack = getCookie("selectedCard");
 
     imageFiles.forEach(function(imageName)
     {
@@ -80,7 +67,7 @@ document.getElementById("updateCard").addEventListener("click", function(event)
     //         img.classList.add('selected-card-cookie');
     // });
 
-    document.cookie = "selectedCard=" + selectedCard + ";path=/; max-age=" + (60 * 60 * 24 * 365);  // 1 year
+    setCookie("selectedCard", selectedCard)
     var msg = "Card backing successfully updated to "+selectedCard.split('.').slice(0, -1).join('.');
     populateInfoMessage("#playerCardsPage #infoMessage", "Card backing changed", msg);
     window.scrollTo({top: 0, behavior: 'smooth'})
