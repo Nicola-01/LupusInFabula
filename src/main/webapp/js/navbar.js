776890804443ce8953238604a4d860fa05ca8add
JapoMomi1 <jacopo.momesso@studenti.unipd.it>
2024-06-06 21:13:57 +0200

    Fixed some colors and added the theme buttons remove on click

--- src/main/webapp/js/navbar.js
+++ src/main/webapp/js/navbar.js
@@ -23,11 +23,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //Buttons theme creation
     const themeButton = document.getElementById('theme-button');
+    //Login and Logout buttons
     const logoutLink = document.getElementById('logout');
     const loginLink = document.getElementById('login');
+    //Hamburger menu element (the input tag)
     const menuOpenCheckbox = document.getElementById('menu-open');
+    //If themes buttons are displayed = true, otherwise = false
     let circlesCreated = false;
-
+    //Set theme
     const theme = getCookie("theme");
 
     themeButton.addEventListener('click', function () {
@@ -36,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const newItem = document.createElement('a');
                 newItem.classList.add('menu-item', 'menu-item-new');
 
+                //Creation of the 4 links for theme selection
                 switch (i) {
                     case 3:
                         newItem.setAttribute('theme', 'dynamic');
@@ -50,19 +54,20 @@ document.addEventListener('DOMContentLoaded', function () {
                             newItem.classList.add('active');
                         break;
                     case 1:
-                        newItem.setAttribute('theme', 'light');
-                        newItem.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="15" viewBox="0 0 512 512" style="vertical-align: unset;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>';
-                        if (theme === 'light')
-                            newItem.classList.add('active');
-                        break;
-                    case 0:
                         newItem.setAttribute('theme', 'dark');
                         newItem.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="15" viewBox="0 0 384 512" style="vertical-align: unset;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>';
                         if (theme === 'dark')
                             newItem.classList.add('active');
                         break;
+                    case 0:
+                        newItem.setAttribute('theme', 'light');
+                        newItem.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24" width="15" viewBox="0 0 512 512" style="vertical-align: unset;"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M361.5 1.2c5 2.1 8.6 6.6 9.6 11.9L391 121l107.9 19.8c5.3 1 9.8 4.6 11.9 9.6s1.5 10.7-1.6 15.2L446.9 256l62.3 90.3c3.1 4.5 3.7 10.2 1.6 15.2s-6.6 8.6-11.9 9.6L391 391 371.1 498.9c-1 5.3-4.6 9.8-9.6 11.9s-10.7 1.5-15.2-1.6L256 446.9l-90.3 62.3c-4.5 3.1-10.2 3.7-15.2 1.6s-8.6-6.6-9.6-11.9L121 391 13.1 371.1c-5.3-1-9.8-4.6-11.9-9.6s-1.5-10.7 1.6-15.2L65.1 256 2.8 165.7c-3.1-4.5-3.7-10.2-1.6-15.2s6.6-8.6 11.9-9.6L121 121 140.9 13.1c1-5.3 4.6-9.8 9.6-11.9s10.7-1.5 15.2 1.6L256 65.1 346.3 2.8c4.5-3.1 10.2-3.7 15.2-1.6zM160 256a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zm224 0a128 128 0 1 0 -256 0 128 128 0 1 0 256 0z"/></svg>';
+                        if (theme === 'light')
+                            newItem.classList.add('active');
+                        break;
                 }
 
+                //Adding the themes created elements after the login or logout element
                 if (logoutLink !== null)
                     logoutLink.parentNode.insertBefore(newItem, logoutLink.nextSibling);
                 if (loginLink !== null)
@@ -93,21 +98,41 @@ document.addEventListener('DOMContentLoaded', function () {
             circlesCreated = true;
 
         } else {
-            const newItems = document.querySelectorAll('.menu-item-new');
-            newItems.forEach(item => item.remove());
-            circlesCreated = false;
+            //Remove of the created theme elements
+            removeThemeElements();
         }
     });
 
     menuOpenCheckbox.addEventListener('click', function () {
         if (circlesCreated) {
-            const newItems = document.querySelectorAll('.menu-item-new');
-            newItems.forEach(item => item.remove());
-            circlesCreated = false;
+            //Remove of the created theme elements
+            removeThemeElements();
         }
     });
 
+    //Remove of the created theme elements when i click outside
+    document.addEventListener('click', handleClickOutside);
+
+    function removeThemeElements() {
+        const newItems = document.querySelectorAll('.menu-item-new');
+        newItems.forEach(item => item.remove());
+        circlesCreated = false;
+    }
+
+    function handleClickOutside(event) {
+        // const themeButton = document.getElementById('theme-button');
+        const createdThemeElements = document.querySelectorAll(".menu-item-new");
+
+        const isClickInsideThemeButton = themeButton.contains(event.target);
+        const isClickInsideThemeElement = Array.from(createdThemeElements).some(item => item.contains(event.target));
+
+        if (!isClickInsideThemeButton && !isClickInsideThemeElement) {
+            removeThemeElements();
+        }
+    }
+
 });
+
 
 const responsiveThemeElements = document.querySelectorAll("#theme a");
 
